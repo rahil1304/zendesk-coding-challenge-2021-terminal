717f0faa16533665d4427e361f2171946928865f
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-23 01:19:49 -0500

    added details for displaying each ticekt

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,9 @@ const yargs = require("yargs");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const _ = require("underscore");
+const chalk = require("chalk");
+const boxen = require("boxen");
+
 dotenv.config();
 
 // const options = yargs
@@ -21,7 +24,17 @@ dotenv.config();
 //   }).argv;
 
 const greeting = `Hello you can view your tickets here!`;
-console.log(greeting);
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555",
+};
+const msgBox = boxen(greeting, boxenOptions);
+
+console.log(msgBox);
 
 var config = {
   method: "get",
@@ -36,22 +49,47 @@ axios(config)
   .then(function (response) {
     console.log(`There are a totol of ${response.data.tickets.length} tickets`);
     let tickets = [];
-    function Ticket(id, subject, description) {
+    function Ticket(id, subject, description, date, status) {
       this.id = id;
       this.subject = subject;
       this.description = description;
+      this.date = date;
+      this.status = status;
     }
 
     _.each(response.data.tickets, function (ticket) {
+      //   console.log(ticket);
       tickets.push(
         new Ticket(
           ticket.id,
           ticket.subject,
-          ticket.description.substring(1, 20)
+          ticket.description.substring(0, 20),
+          ticket.created_at,
+          ticket.status
         )
       );
     });
     console.table(tickets);
+
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    readline.question(
+      `Please enter the id of the ticket you want to view in detail: `,
+      (id) => {
+        console.log(`<------------------------------------>`);
+        console.log(`These are the details of Ticket ${id}!`);
+        console.log(`Subject: ${tickets[id - 1].subject}`);
+        console.log(`Description: ${tickets[id - 1].description}`);
+        console.log(`Date: ${tickets[id - 1].date}`);
+        console.log(`Status: ${tickets[id - 1].status}`);
+        console.log(`<------------------------------------>`);
+        // console.log(`Subject: ${tickets[id - 1].subject}`);
+        readline.close();
+      }
+    );
   })
   .catch(function (error) {
     console.log(error);
