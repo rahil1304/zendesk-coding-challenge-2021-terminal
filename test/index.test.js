72dad96be385286027e3665a48612b6d3b91ae3e
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-27 04:49:12 -0500

    added unit test for getTicketsAPI

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,11 @@
-const { createNewTickets } = require("../bin/index");
+const { createNewTickets, getTicketsAPI } = require("../bin/index");
 
 // Empty Unit Test
 test("On calling getTicketsAPI, the list of tickets should be displayed", () => {
   expect(createNewTickets([])).toEqual(expect.arrayContaining([]));
 });
 
-exampleTicket = {
+let exampleTicket = {
   requester_id: 1,
   assignee_id: 5,
   subject: "velit eiusmod reprehenderit officia cupidatat",
@@ -14,9 +14,25 @@ exampleTicket = {
   tags: ["est", "nisi", "incididunt"],
 };
 
+let config = {
+  method: "get",
+  url: "https://zccstudentsnov2021.zendesk.com/api/v2/tickets.json",
+  headers: {
+    Authorization: process.env.AUTHORIZATION,
+    Cookie: process.env.COOKIE,
+  },
+};
+
 // single ticket test
 test("On calling getTicketsAPI, the list of tickets should be displayed", () => {
   expect(createNewTickets([exampleTicket])).toEqual(
     expect.arrayContaining([exampleTicket])
+  );
+});
+
+// get tickets API test
+test("On calling getTicketsAPI, the list of tickets should be displayed", () => {
+  expect(getTicketsAPI(config, [exampleTicket])).toEqual(
+    expect.arrayContaining([])
   );
 });
