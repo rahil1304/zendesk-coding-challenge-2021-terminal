f05edbf1b84019e5f52fdd44f5f99c621d68126d
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-27 03:59:58 -0500

    added unit test for ticket

--- test/index.test.js
+++ test/index.test.js
@@ -4,3 +4,19 @@ const { createNewTickets } = require("../bin/index");
 test("On calling getTicketsAPI, the list of tickets should be displayed", () => {
   expect(createNewTickets([])).toEqual(expect.arrayContaining([]));
 });
+
+exampleTicket = {
+  requester_id: 1,
+  assignee_id: 5,
+  subject: "velit eiusmod reprehenderit officia cupidatat",
+  description:
+    "Aute ex sunt culpa ex ea esse sint cupidatat aliqua ex consequat sit reprehenderit. Velit labore proident quis culpa ad duis adipisicing laboris voluptate velit incididunt minim consequat nulla. Laboris adipisicing reprehenderit minim tempor officia ullamco occaecat ut laborum.\n\nAliquip velit adipisicing exercitation irure aliqua qui. Commodo eu laborum cillum nostrud eu. Mollit duis qui non ea deserunt est est et officia ut excepteur Lorem pariatur deserunt.",
+  tags: ["est", "nisi", "incididunt"],
+};
+
+// single ticket test
+test("On calling getTicketsAPI, the list of tickets should be displayed", () => {
+  expect(createNewTickets([exampleTicket])).toEqual(
+    expect.arrayContaining([exampleTicket])
+  );
+});
